46b9bc32416b8e64691f6fc2a71371b3f7fae5bb
GoutamiGajakosh96 <goutamigajakosh96@gmail.com>
2024-07-01 16:39:24 +0530

    Update task.tf

--- teraform/task.tf
+++ teraform/task.tf
@@ -4,19 +4,19 @@ resource "aws_ecs_task_definition" "task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role_1.arn
 
 
   container_definitions = <<DEFINITION
   [
     {
       "name": "hello-world",
-      "image": "211125733933.dkr.ecr.us-east-2.amazonaws.com/hello-world-njs:latest",
+      "image": "211125733933.dkr.ecr.us-east-2.amazonaws.com/hello-world-sb:latest",
       "essential": true,
       "portMappings": [
         {
-          "containerPort": 3000,
-          "hostPort": 3000
+          "containerPort": 8080,
+          "hostPort": 8080
         }
       ]
     }
